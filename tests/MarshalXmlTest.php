37fc0ea2154c4d422759766fbe6a25ccdc325131
Martin Ossowski <martin.ossowski@googlemail.com>
2017-12-21 00:42:59 +0100

    fix floating value in PHP 7

--- tests/MarshalXmlTest.php
+++ tests/MarshalXmlTest.php
@@ -69,7 +69,7 @@ class MarshalXmlTest extends TestCase {
     private function createUser() {
         $user            = new \stdClass();
         $user->id        = 123;
-        $user->score     = round(3.4, 1);
+        $user->score     = 3.0;
         $user->email     = 'kingson@example.org';
         $user->nicknames = ['pfefferkuchenmann', 'lululu'];
 
