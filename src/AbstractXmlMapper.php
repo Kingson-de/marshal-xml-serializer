08281f73fe1193dd1a027872c85a7f505f55ede9
Martin Ossowski <martin.ossowski@googlemail.com>
2017-12-21 14:58:00 +0100

    add implementation comment to AbstractXmlMapper

--- src/AbstractXmlMapper.php
+++ src/AbstractXmlMapper.php
@@ -4,6 +4,10 @@ declare(strict_types = 1);
 
 namespace KingsonDe\Marshal;
 
+/**
+ * Concrete Mapper classes MUST implement a "map" function that must return an array or null.
+ * The reason of not having an abstract function for the "map" function is type hinting.
+ */
 class AbstractXmlMapper extends AbstractMapper {
 
     protected function attributes(): string {
